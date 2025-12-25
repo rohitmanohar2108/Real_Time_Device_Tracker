bdc35c80f5c9da1f48b1ecee3ee604552b0d8c4f
Rohit Manohar <manoharrohit372@gmail.com>
2025-12-25 22:54:28 +0530

    set up EJS

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express();
 
 const http = require("http");
+const path = require('path');
 
 const  socketio = require("socket.io");
 const server = http.createServer(app);
 const io = socketio(server);
+
+app.set("view engin", "ejs");
+app.set(express.static(path.join(__dirname, "public")));
 
 
 
